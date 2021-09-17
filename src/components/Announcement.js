b94a094d5f9c03e84eed1d52e02511e34e2a7dcf
kyungho <epsoe@naver.com>
2021-09-17 12:52:42 +0900

    UI

---
+++ src/components/Announcement.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  height: 30px;
+  background-color: teal;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
+  font-weight: 500;
+`;
+
+const Announcement = () => {
+  return <Container>신규 가입시, 최대 15만원 할인 쿠폰팩</Container>;
+};
+
+export default Announcement;
